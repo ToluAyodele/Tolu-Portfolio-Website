921d083a5a9c639307aa00337c396be4c4af76d5
ToluAyodele <Toluwanimi.ayodele01@gmail.com>
2024-05-03 06:36:49 -0400

    Completed Navbar

---
+++ src/layouts/Navbar.jsx
@@ -0,0 +1,26 @@
+import "./navbar.css";
+
+
+const Navbar = () =>{
+    return(
+        <>
+            <nav className="desktop-nav">
+                <div className="logo">
+                    Toluwanimi Ayodele
+                </div>
+
+                <div>
+                    <ul className="nav-links">
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#experience">Experience</a></li>
+                        <li><a href="#projects">Projects</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                    </ul>
+                    
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
